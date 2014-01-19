db3c5686d2c7bd77b08ac09daf66e150ee660794
Martin Green <martin.speleo@gmail.com>
2014-01-19 20:16:16 +0000

    Added expansion function.  (Not fully tested.)

--- src/points.py
+++ src/points.py
@@ -6,9 +6,10 @@ import dicom
 import numpy
 from octrees.octrees import Octree
 import os, os.path
+from math import sin, cos, pi, asin
 
-SAMPLING = 4
-POISSON_DEPTH = 4
+SAMPLING = 2
+POISSON_DEPTH = 8
 
 mypath = "../data/redfred"
 myseries = "1.2.840.113704.1.111.3736.1370522307.4"
@@ -157,24 +158,63 @@ def copy_point_cloud_excluding(points, excluding_points, distance):
             clean_skin.insert(point, normal)
     return clean_skin
 
+def expand_bounds(((xl, xu), (yl, yu), (zl, zu)), distance):
+    return ((xl - distance, xu + distance), (yl - distance, yu + distance), (zl - distance, zu + distance))
+
 def expand(points, distance):
-    print points.bounds
-    clean_skin = Octree(points.bounds)#expand bounds
+    expanded = Octree(expand_bounds(points.bounds, distance))#expand bounds
     points_list = points.by_distance_from_point((0,0,0)) #Points do not actually need to be ordered, perhaps they can be found more efficiently
-    for ignore, point, normal in points_list:        
+    d_angle = 0.2
+    max_angle = 0.600000001
+    all_norm_offsets = [(0, 0, 1)]
+    distance_slightly_reduced = 0.99999 * distance
+    for theta in [d_angle * (i + 1) for i in range(int(max_angle / d_angle))]:
+        sin_theta = sin(theta)
+        cos_theta = cos(theta)
+        num_phis = int(round(2 * pi * sin_theta / d_angle))
+        for phi in [2 * pi * j / num_phis for j in range(num_phis)]:
+            all_norm_offsets.append((sin(theta) * cos(phi), sin(theta) * sin(phi), cos(theta)))
+    points_list = list(points_list)
+    num_points = len(points_list) 
+    print num_points
+    hits = 0
+    for i, (ignore, point, normal) in enumerate(points_list):
+        if i % 100 == 0:
+            print "%0.2f%% complete of %i hits from %i starting points" % ((100.0 * i) / num_points, hits, i)
+        #Determine transform to rotate norm offsets on actual norms
+        #Need to rotate by angle around the vector(u, v, 0)
+        sin_angle = (normal[0] ** 2 + normal[1] ** 2) ** 0.5
         try:
-            points.near_point(point, distance).next()
-        except StopIteration:
-            clean_skin.insert(point, normal)
+            angle = asin(sin_angle)
+        except:
+            angle = asin(1)
+        cos_angle = cos(angle)
+        one_minus_cos_angle = 1 - cos_angle
+        u = -normal[1] / sin_angle
+        v = normal[0] / sin_angle 
+        for x, y, z in all_norm_offsets:
+            ux_plus_vy = (u * x + v * y)
+            nx = u * ux_plus_vy * one_minus_cos_angle + x * cos_angle + v * z * sin_angle
+            ny = v * ux_plus_vy * one_minus_cos_angle + y * cos_angle - u * z * sin_angle
+            nz = z * cos_angle + (-v * x + u * y) * sin_angle
+            normal = nx, ny, nz
+            new_point = [p + n * distance for p, n in zip(point, normal)]
+            try:
+                points.near_point(new_point, distance_slightly_reduced).next()
+            except StopIteration:
+                expanded.insert(new_point, normal)
+                hits += 1
+    return expanded
 
 if __name__ == '__main__':
     points = load(mypath, myseries, [500, 1200], SAMPLING)
     #Make clean skin point cloud by removing any skin points (density 500) that are near bone/metal points (density 12000)
     clean_skin = copy_point_cloud_excluding(points[500], points[1200], 2)
-    #expanded_skin = expand(clean_skin, 10)
+    expanded_skin = expand(clean_skin, 10)
     
     #Run Poisson reconstruction alogrithem on point clouds
     make_ply(points[500], OUTPUT_FILENAME_BASE + "skin", poisson_depth = POISSON_DEPTH)
     make_ply(clean_skin, OUTPUT_FILENAME_BASE + "clean", poisson_depth = POISSON_DEPTH)
     make_ply(points[1200], OUTPUT_FILENAME_BASE + "bone", poisson_depth = POISSON_DEPTH)
+    make_ply(expanded_skin, OUTPUT_FILENAME_BASE + "expand", poisson_depth = POISSON_DEPTH)
 
