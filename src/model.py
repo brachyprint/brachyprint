bdc61eb19bb0a83bc813b856904227fa388d63a6
Olly Madge <oghm2-github@srcf.net>
2014-01-26 13:15:02 +0000

    Added extra functionality to the Vector class, and changed Vertex to inherit it. Added tests for the Vector class.

--- src/model.py
+++ src/model.py
@@ -16,7 +16,11 @@ class AStar:
             self.d[node] = dist, dist + ((node.x - self.s2[0]) ** 2 + (node.y - self.s2[1]) ** 2 + (node.z - self.s2[2]) ** 2) * 0.5
         
 
-class Mesh:
+class Mesh(object):
+    '''
+    Class representing a mesh.
+    '''
+
     def __init__(self):
         self.vertices = []
         self.faces = []
@@ -25,12 +29,15 @@ class Mesh:
         self.maxY, self.minY = None, None
         self.maxZ, self.minZ = None, None
         self.volumes = {}
+        self.next_vertex_name = 0
         self.next_face_name = 0
         self.next_volume_name = 0
         self.face_to_vol = {}
 
     def add_vertex(self, x, y, z):
-        self.vertices.append(Vertex(x, y, z))
+        v = Vertex(x,y,z, self.next_vertex_name)
+        self.next_vertex_name += 1
+        self.vertices.append(v)
         if self.maxX is None:
             self.maxX = x
             self.minX = x
@@ -44,6 +51,7 @@ class Mesh:
         elif y < self.minY: self.minY = y
         if z > self.maxZ: self.maxZ = z
         elif z < self.minZ: self.minZ = z
+        return v
 
     def add_face(self, v1, v2, v3):
         f = Face(self.next_face_name, v1, v2, v3)
@@ -63,6 +71,9 @@ class Mesh:
             v.add_face(f)
   
     def allocate_volumes(self):
+        '''Allocate each face to a particular volume.
+
+        '''
         to_grow = []
         for face in self.faces:
             if face.volume is None:
@@ -82,58 +93,211 @@ class Mesh:
                                 to_grow.append(new_face)
         
         
+class Vector(object):
+    '''
+    Class representing a vector.
+    '''
 
-class Vertex:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.edges = []
-        self.faces = []
-
-    def add_edge(self, edge):
-        self.edges.append(edge)
-
-    def add_face(self, face):
-        self.faces.append(face)
-
-    def __sub__(self, v):
-        return Vector(self.x - v.x, self.y - v.y, self.z - v.z)
-
-    def normal(self):
-        n = sum([f.normal for f in self.faces], nullVector)
-        return n.normalise()
-
-    def adjacent_verticies(self):
-        return [e.v1 for e in self.edges if e.v2 is self] + [e.v2 for e in self.edges if e.v1 is self]
-
-class Vector:
     def __init__(self, x, y, z):
         self.x, self.y, self.z = x, y, z
+
+    def __eq__(self, v):
+        if isinstance(v, Vector):
+            return self.x == v.x and self.y == v.y and self.z == v.z
+        elif isinstance(v, list):
+            return self.x == v[0] and self.y == v[1] and self.z == v[2]
+        else:
+            raise NotImplementedError
+
+    def __gt__(self, v):
+        raise TypeError("Ambiguous meaning for a Vector")
+
+    def __lt__(self, v):
+        raise TypeError("Ambiguous meaning for a Vector")
+
+    def __lshift__(self, other):
+        raise TypeError("Vector is not a binary type")
+
+    def __rshift__(self, other):
+        raise TypeError("Vector is not a binary type")
+
+    def __and__(self, other):
+        raise TypeError("Vector is not a binary type")
+
+    def __xor__(self, other):
+        raise TypeError("Vector is not a binary type")
+
+    def __or__(self, other):
+        raise TypeError("Vector is not a binary type")
+
+    def __add__(self, v):
+        if isinstance(v, Vector):
+            return Vector(self.x + v.x, self.y + v.y, self.z + v.z)
+        else:
+            raise NotImplementedError
+
+    def __sub__(self, v):
+        if isinstance(v, Vector):
+            return Vector(self.x - v.x, self.y - v.y, self.z - v.z)
+        else:
+            raise NotImplementedError
+
+    def __mul__(self, val):
+        if isinstance(val, float) or isinstance(val, int):
+            return Vector(self.x*val, self.y*val, self.z*val)
+        else:
+            raise NotImplementedError
+
+    def __rmul__(self, other):
+        self.__mul__(other)
+
+    def __div__(self, val):
+        if isinstance(val, float) or isinstance(val, int):
+            return Vector(self.x/val, self.y/val, self.z/val)
+        else:
+            raise NotImplementedError
+
+    def __rdiv__(self, other):
+        self.__div__(other)
+
+    def __truediv__(self, val):
+        if isinstance(val, float) or isinstance(val, int):
+            return Vector(self.x/val, self.y/val, self.z/val)
+        else:
+            raise NotImplementedError
+
+    def __rtruediv__(self, other):
+        self.__truediv__(other)
+
+    def __iadd__(self, v):
+        if isinstance(v, Vector):
+            self.x += v.x
+            self.y += v.y
+            self.z += v.z
+            return self
+        else:
+            raise NotImplementedError
+        
+    def __isub__(self, v):
+        if isinstance(v, Vector):
+            self.x -= v.x
+            self.y -= v.y
+            self.z -= v.z
+            return self
+        else:
+            raise NotImplementedError
+
+    def __imul__(self, val):
+        if isinstance(val, float) or isinstance(val, int):
+            self.x *= val
+            self.y *= val
+            self.z *= val
+            return self
+        else:
+            raise NotImplementedError
+
+    def __getitem__(self, i):
+        if i == 0:
+            return self.x
+        elif i == 1:
+            return self.y
+        elif i == 2:
+            return self.z
+        else:
+            raise IndexError
+
+    def __len__(self):
+        return 3
+
+    def __repr__(self):
+        return "<Vector x:%f y:%f z:%f>" % (self.x, self.y, self.z)
+
+    def __str__(self):
+        return "From str method of Vector: x is %f, y is %f, z is %f" % (self.x, self.y, self.z)
+
     def cross(self, v):
         return Vector(self.y * v.z - self.z * v.y,
                       self.z * v.x - self.x * v.z,
                       self.x * v.y - self.y * v.x)
-    def __add__(self, v):
-        return Vector(self.x + v.x, self.y + v.y, self.z + v.z)
 
     def normalise(self):
         m = (self.x ** 2 + self.y ** 2 + self.z ** 2) ** 0.5
-        return Vector(self.x/m, self.y/m, self.z/m)
+        if m == 0.0:
+            return Vector(self.x, self.y, self.z)
+        else:
+            return Vector(self.x/m, self.y/m, self.z/m)
+
 
 nullVector = Vector(0, 0, 0)
 
+
+class Vertex(Vector):
+    '''
+    A class representing a mesh vertex.
+    '''
+
+    def __init__(self, x, y, z, name):
+        self.x, self.y, self.z = x, y, z
+        self.name = name
+        self.edges = []
+        self.faces = []
+
+    def __repr__(self):
+        return "<Vertex x:%f y:%f z:%f name:%d>" % (self.x, self.y, self.z, self.name)
+
+    def __str__(self):
+        return "From str method of Vertex: x is %f, y is %f, z is %f, name is %d" % (self.x, self.y, self.z, self.name)
+
+    def add_edge(self, edge):
+        '''
+        Associate an edge with the vertex.
+        '''
+        self.edges.append(edge)
+
+    def add_face(self, face):
+        '''
+        Associate a face with the vertex.
+        '''
+        self.faces.append(face)
+
+    def tovector(self):
+        return Vector(self.x, self.y, self.z)
+
+    def normal(self):
+        '''
+        Determine a normal at the vertex by averaging its associated face normals.
+        '''
+        n = sum([f.normal for f in self.faces], nullVector)
+        return n.normalise()
+
+    def adjacent_vertices(self):
+        '''
+        Return all adjacent vertices.
+        '''
+        return [e.v1 for e in self.edges if e.v2 is self] + [e.v2 for e in self.edges if e.v1 is self]
+
+
 class Face:
+    '''
+    A class representing a mesh face.
+    '''
+
     def __init__(self, name, v1, v2, v3):
         self.name = name
         self.vertices = v1, v2, v3
         self.normal = (v1 - v2).cross(v1 - v3)
         self.volume = None
         self.edges = []
+
     def add_edge(self, edge):
         self.edges.append(edge)
 
+
 class Edge:
+    '''
+    A class representing a mesh edge.
+    '''
+
     def __init__(self, v1, v2):
         self.v1, self.v2 = v1, v2
         self.faces = []
@@ -143,8 +307,6 @@ class Edge:
     def add_face(self, face):
         self.faces.append(face)
     
-    #def normal(self):
-    #    sum([f.normal for f in self.faces])
         
 def makeMesh(ply):
     mesh = Mesh()
