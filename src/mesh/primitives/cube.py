f80a5521eb47bed89a7a5cc423d90fb1e443d445
Olly Madge <oghm2-github@srcf.net>
2014-02-07 16:56:13 +0000

    Use new style objects. Added a "cube" geometrical primitive, plus commented code to display it.

---
+++ src/mesh/primitives/cube.py
@@ -0,0 +1,20 @@
+
+from __future__ import division
+
+def make_cube(c, d, offset=[0,0,0]):
+    '''
+    Add a cube to an existing mesh.
+    '''
+
+    # create the 8 corner vertices
+    vs = [(0,0,0), (0,0,d), (d,0,0), (d,0,d), (d,d,0), (d,d,d), (0,d,0), (0,d,d)]
+    vertices = [c.add_vertex(x+offset[0], y+offset[1], z+offset[2]) for x,y,z in vs]
+
+    # create side faces
+    for i in range(len(vertices)):
+        c.add_face(vertices[i-2], vertices[i-1], vertices[i])
+
+    # create end faces
+    for i in [0, 1, 4, 5]:
+        c.add_face(vertices[i-2], vertices[i], vertices[i+2])
+
