1f36c1abed10b2a366fb036fc7cb07aa53a35db6
Olly Madge <oghm2-github@srcf.net>
2014-02-13 18:22:06 +0000

    Added binary STL output format.

--- src/mesh/fileio/stl.py
+++ src/mesh/fileio/stl.py
@@ -24,6 +24,9 @@ class StlWriter(object):
         self.outputformat = outputformat
     
     def write(self, m, name, filename):
+        '''
+        Write the file in the specified format.
+        '''
         if self.outputformat == STL_FORMAT_ASCII:
             self.write_ascii(m, name, filename)
         elif self.outputformat == STL_FORMAT_BINARY:
@@ -51,7 +54,27 @@ class StlWriter(object):
             fp.write("endsolid %s\n" % (name))
     
     def write_binary(self, m, name, filename):
-        raise NotImplementedError()
+        with open(filename, 'wb') as fp:
+            # write the header
+            count = 0
+            fp.write(struct.pack("80sI", b'Mesh binary STL writer', count))
+
+            for f in m.faces:
+                n = f.normal.normalise()
+                v = f.vertices
+
+                face = [   n[0],    n[1],    n[2],
+                        v[0][0], v[0][1], v[0][2],
+                        v[1][0], v[1][1], v[1][2],
+                        v[2][0], v[2][1], v[2][2],
+                        0 ]
+                fp.write(struct.pack("12fH", *face))
+
+                count = count + 1
+
+            # overwrite the header with the number of faces output
+            fp.seek(0)
+            fp.write(struct.pack("80sI", b'Mesh Binary STL Writer', count))
 
 
 class StlReader(object):
@@ -64,7 +87,7 @@ class StlReader(object):
     def __init__(self):
         pass
 
-    def read(self, filename):
+    def read(self, m, filename):
         with open(filename, 'rb') as fp:
 
             fileContent = fp.read()
@@ -74,32 +97,43 @@ class StlReader(object):
 
             # read the header
             if fileContent[0:4] == "80sI":
-                self.decode_binary(fileContent)
+                self.decode_binary(m, fileContent)
             elif fileContent[0:5] == "solid":
-                self.decode_ascii(fileContent)
+                self.decode_ascii(m, fileContent)
             else:
                 raise IOError("Invalid STL file")
 
-    def decode_ascii(self, data):
+    def decode_ascii(self, m, data):
         #while i < len(data)
             # read a line
             
         raise NotImplementedError()
 
-    def decode_binary(self, data):
+    def decode_binary(self, m, data):
         raise NotImplementedError()
 
 
-def write_stl(m, filename, name="mesh"):
+def write_stl(m, filename, name="mesh", fileformat=STL_FORMAT_ASCII):
     '''
-    Write a mesh to an STL file.
+    Write a Mesh object to an STL file.
+
+    Example:
+        import mesh
+        m = mesh.Mesh()
+        mesh.primitives.make_cylinder(m, 10, 100, 10)
+        mesh.fileio.write_stl(m, "cylinder.stl", fileformat=mesh.fileio.STL_FORMAT_BINARY)
     '''
-    StlWriter().write(m, name, filename)
+    StlWriter(fileformat).write(m, name, filename)
 
 
-def read_stl(filename):
+def read_stl(m, filename):
     '''
-    Read an STL file and create a mesh from it.
+    Read an STL file and add it to a Mesh object.
+
+    Example:
+        import mesh
+        m = mesh.Mesh()
+        mesh.fileio.read_stl(m, "cylinder.stl")
     '''
-    return StlReader().read(filename)
+    return StlReader().read(m, filename)
 
